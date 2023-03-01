3d033f17f783b50d7ac9c96b4c8d4009942958f5
Timur <timur87@mail.ru>
2023-03-01 04:28:27 +0300

    phpserver added

--- src/js/script.js
+++ src/js/script.js
@@ -46,4 +46,36 @@ $(document).ready(function () {
   }
   toggleSlide(".catalog-item__link");
   toggleSlide(".catalog-item__back");
+
+  // Modal
+
+  $("[data-modal=consultation]").on("click", function () {
+    $(".overlay, #consultation").fadeIn("slow");
+  });
+  $(".modal__close").on("click", function () {
+    $(".overlay, #consultation, #thanks, #order").fadeOut("slow");
+  });
+
+  $(".button_mini").each(function (i) {
+    $(this).on("click", function () {
+      $("#order .modal__descr").text($(".catalog-item__subtitle").eq(i).text());
+      $(".overlay, #order").fadeIn("slow");
+    });
+  });
+
+  $("form").submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize(),
+    }).done(function () {
+      $(this).find("input").val("");
+      $("#consultation, #order").fadeOut();
+      $(".overlay, #thanks").fadeIn("slow");
+
+      $("form").trigger("reset");
+    });
+    return false;
+  });
 });
